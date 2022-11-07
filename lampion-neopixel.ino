4c733817a1f43ff6bd72e213bd919576c4cf70c7
aquacat <aquacat@decade.cz>
2022-11-07 20:21:46 +0100

    Implement effects 2 and 3

--- lampion-neopixel.ino
+++ lampion-neopixel.ino
@@ -6,103 +6,187 @@
 #define EFFECT_COUNT 7
 #define BUTTON_DEBOUNCE_MS 500
 
+Adafruit_NeoPixel strip(15, 2, NEO_GRB + NEO_KHZ800);
+
 typedef struct {
-  float r;
-  float g;
-  float b;
+	float r;
+	float g;
+	float b;
 } RGBColor;
 
 RGBColor cross_fade_get_step(RGBColor c_from, RGBColor c_to, u16 steps) {
-  RGBColor step;
-  
-  step.r = (c_to.r - c_from.r) / steps;
-  step.g = (c_to.g - c_from.g) / steps;
-  step.b = (c_to.b - c_from.b) / steps;
+	RGBColor step;
+	
+	step.r = (c_to.r - c_from.r) / steps;
+	step.g = (c_to.g - c_from.g) / steps;
+	step.b = (c_to.b - c_from.b) / steps;
 
-  return step;
+	return step;
 }
 
 void color_add(RGBColor *col, RGBColor step) {
-  col->r += step.r;
-  col->g += step.g;
-  col->b += step.b;
+	col->r += step.r;
+	col->g += step.g;
+	col->b += step.b;
 }
 
-Adafruit_NeoPixel strip(15, 2, NEO_GRB + NEO_KHZ800);
+void strip_fill_rows(RGBColor *columns) {
+	for (u8 i = 0; i < 5; i++) {
+		strip.setPixelColor(i, columns[i].r, columns[i].g, columns[i].b);
+		strip.setPixelColor(9 - i, columns[i].r, columns[i].g, columns[i].b);
+		strip.setPixelColor(10 + i, columns[i].r, columns[i].g, columns[i].b);
+	}
+}
 
 u8 effect_id = 0;
 u32 button_press_time = 0;
 
 //
-// Effect 1 data
+// Effect 1 - Fading rainbow
+// There are 6 predefined ("target") RGB colors and cross-fade between them.
+// One color fills up the whole LED strip.
 //
 
-RGBColor effect_1_colors[] = {
-  {255, 200, 0},
-  {255, 0, 0},
-  {255, 0, 200},
-  {0, 0, 255},
-  {0, 255, 255},
-  {0, 255, 0}
+RGBColor effect_1_3_colors[] = {
+	{255, 200, 0},
+	{255, 0, 0},
+	{255, 0, 200},
+	{0, 0, 255},
+	{0, 255, 255},
+	{0, 255, 0}
 };
 
-u8 effect_1_color_count = sizeof(effect_1_colors) / sizeof(RGBColor);
+u8 effect_1_3_color_count = sizeof(effect_1_3_colors) / sizeof(RGBColor);
 
 struct {
-  RGBColor current;
-  RGBColor step;
-  u16 steps_remaining;
-  u8 color_target;
+	RGBColor current;
+	RGBColor step;
+	u16 steps_remaining;
+	u8 color_target;
 } effect_1_state = {
-  .current = {0, 0, 0},
-  .step = {0, 0, 0},
-  .steps_remaining = 0,
-  .color_target = effect_1_color_count - 1
+	.current = {0, 0, 0},
+	.step = {0, 0, 0},
+	.steps_remaining = 0,
+	.color_target = effect_1_3_color_count - 1
 };
 
 void effect_1_update() {
-  if (effect_1_state.steps_remaining == 0) {
-    effect_1_state.color_target = (effect_1_state.color_target + 1) % effect_1_color_count;
-    effect_1_state.steps_remaining = 240; // 240 steps = 60 Hz * 4 s
-    effect_1_state.step = cross_fade_get_step(effect_1_state.current, effect_1_colors[effect_1_state.color_target], effect_1_state.steps_remaining);
-    delay(1600);
-  }
+	if (effect_1_state.steps_remaining == 0) {
+		effect_1_state.color_target = (effect_1_state.color_target + 1) % effect_1_3_color_count;
+		effect_1_state.steps_remaining = 240; // 240 steps = 60 Hz * 4 s
+		effect_1_state.step = cross_fade_get_step(effect_1_state.current, effect_1_3_colors[effect_1_state.color_target], effect_1_state.steps_remaining);
+		delay(1600);
+	}
 
-  color_add(&effect_1_state.current, effect_1_state.step);
-  effect_1_state.steps_remaining--;
+	color_add(&effect_1_state.current, effect_1_state.step);
+	effect_1_state.steps_remaining--;
 
-  strip.fill(Adafruit_NeoPixel::Color(effect_1_state.current.r, effect_1_state.current.g, effect_1_state.current.b));
-  delay(16);
+	strip.fill(Adafruit_NeoPixel::Color(effect_1_state.current.r, effect_1_state.current.g, effect_1_state.current.b));
+	delay(16);
 }
 
 void setup() {
-  Serial.begin(115200);
-  digitalWrite(3, HIGH);
-  attachInterrupt(digitalPinToInterrupt(3), switch_effect, FALLING);
-  
-  strip.begin();
-  Serial.print("ready");
-
+	Serial.begin(115200);
+	digitalWrite(3, HIGH);
+	attachInterrupt(digitalPinToInterrupt(3), switch_effect, FALLING);
+	
+	strip.begin();
+	Serial.print("ready");
 }
 
-void loop() {
-  if (effect_id == 0) {
-    effect_1_update();
-  }
-  
- // strip.setPixelColor(0, 255, 0, 0);
+//
+// Effect 2 - Cascade
+// Loop of 6 colors lit up row by row from top to bottom.
+// Each row is lightened up in a different color.
+//
 
-  strip.show();
-  
+struct {
+	u8 row;
+	u8 color_target;
+} effect_2_state = {
+	.row = 0,
+	.color_target = 0
+};
+
+
+void effect_2_update() {
+	RGBColor column[5];
+	
+	for (u8 i = 0; i < 5; i++) { // 5 LEDs (rows)
+		if (i == effect_2_state.row) {
+			column[i] = effect_1_3_colors[effect_2_state.color_target];
+		} else {
+			column[i] = {0, 0, 0}; // black
+		}
+	}
+	
+	effect_2_state.color_target = (effect_2_state.color_target + 1) % effect_1_3_color_count;
+	effect_2_state.row = (effect_2_state.row + 1) % 5;
+	
+	strip_fill_rows(column);
+	delay(350);
+}
+
+//
+// Effect 3 - Blink
+// Loop of 6 colors. Each color is lit up for a specified time duration.
+// One color fills up the whole LED strip.
+//
+
+struct {
+	u8 color_target;
+} effect_3_state = {
+	.color_target = 0 // Starting with the first color in effect_1_3_colors
+};
+
+void effect_3_update() {
+	RGBColor *col = effect_1_3_colors + effect_3_state.color_target;
+	
+	strip.fill(Adafruit_NeoPixel::Color(col->r, col->g, col->b));
+	strip.show();
+	delay(3000);
+	strip.fill(0); // black
+	strip.show();
+	delay(1000);
+	
+	effect_3_state.color_target = (effect_3_state.color_target + 1) % effect_1_3_color_count;
+}
+
+//
+// Effect 4 - Spin
+// Loop of 6 colors lit by columns (there are 3 strip segments).
+// Each column is lighten up in a different color.
+//
+
+//
+// Effect 5 - Knight Rider Cascade
+// Loop of 6 colors. Each color is lighten up by row from bottom up and down two times
+// and than changed to the next color in loop.
+//
+
+void loop() {
+	switch (effect_id) {
+		case 0:
+			effect_1_update();
+			break;
+			
+		case 1:
+			effect_2_update();
+			break;
+		
+		case 2:
+			effect_3_update();
+			break;
+	}
+	
+	strip.show();
 }
 
 void switch_effect() {
-  u32 t = millis();
+	u32 t = millis();
 
-  if (t - button_press_time > BUTTON_DEBOUNCE_MS){
-      effect_id = (effect_id + 1) % EFFECT_COUNT;
-      button_press_time = t;
-  }
-    
-
+	if (t - button_press_time > BUTTON_DEBOUNCE_MS){
+			effect_id = (effect_id + 1) % EFFECT_COUNT;
+			button_press_time = t;
+	}
 }
